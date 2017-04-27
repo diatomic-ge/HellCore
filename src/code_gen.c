bc34b896a8811b1cfa543c0b24351981c6a68173
diatomic.ge <diatomic.ge@gmail.com>
2017-04-26 22:36:11 -0400

    code_gen: Fix bbd_cmp type signature
    bbd_cmp is used only with qsort, but qsort expects a function that takes
    const void pointers.
    Fix bbd_cmp to take const void pointers, like qsort wants, and cast
    inside the function to silence the compiler warning.

--- src/code_gen.c
+++ src/code_gen.c
@@ -1144,9 +1144,9 @@ ref_size(unsigned max)
 
 #ifdef BYTECODE_REDUCE_REF
 static int
-bbd_cmp(unsigned *a, unsigned *b)
+bbd_cmp(const void *a, const void *b)
 {
-	return *a - *b;
+	return *((unsigned*) a) - *((unsigned*) b);
 }
 #endif /* BYTECODE_REDUCE_REF */
 
