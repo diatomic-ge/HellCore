13324c9e7a6d0dbf9a3346a005a79ce123abde7e
diatomic.ge <diatomic.ge@gmail.com>
2026-02-05 13:50:41 -0500

    Add directories for various autoconf files
    Add a build-aux directory to store all the extra scripts autoconf and
    automake add to help the build process without cluttering up the main
    directory.
    
    Additionally, add an m4 directory for any future m4 macros we add, or
    which aclocal installs, so that, e.g., users don't need to have
    autoconf-archive.

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
+# Die on failures.
+set -e
+
 # Regenerate the build system.
 # You probably shouldn't need to modify this.
 
+# Make sure we're in the project directory.
+cd "$(dirname "$0")"
+
+# Make sure we've got an m4 macro directory.
+mkdir -p m4 build-aux
+
+# If we use macros available on the system, copy them into our m4 directory if
+# we don't have them, or if the system has a newer version.
+aclocal --install
+
+# Regenerate our build system.
 autoreconf --install --force
